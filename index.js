b476801f91828d0b11c95d0b6f0c89313df08247
Riadul Islam <riadul551@gmail.com>
2026-05-18 13:32:36 +0600

    make the server and connect to the mongo database

---
+++ index.js
@@ -0,0 +1,43 @@
+// google dns server 
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const uri = process.env.MONGO_URI;
+const port = process.env.PORT;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Wander last");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
